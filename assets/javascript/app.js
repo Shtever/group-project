2f6fabb67e6771dad27e2d4d43079dcf30ec812c
Stephen McMillen <Shtevetm@gmail.com>
2019-09-19 14:29:52 -0500

    Worked a bit on API url

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//
+// https://developers.zomato.com/api/v2.1/cities?q=Austin&apikey=9de42e8f38d437b717a205e52e647b2f
+// *** USE GOOGLE API TO GEOLOCATE BASED ON LATITUDE/LONGITUDE
+
+// restaurant name
+// **** city/loacation
+var location = "Austin"
+// hours
+// **** cuisine type
+var cuisineType = "" 
+// **** price range 
+var price = ""
+// **** meal type (breakfast/lunch/dinner)
+var mealType = ""
+// view menu
+// review score
+// featured tags
+
+var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=" + location + "&apikey=9de42e8f38d437b717a205e52e647b2f"
