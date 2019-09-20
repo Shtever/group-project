7b00aa7e8de59b72664851236e7a0edd84523b16
Stephen McMillen <Shtevetm@gmail.com>
2019-09-20 10:19:12 -0500

    added geolocation api... not functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,26 @@
 //
 // https://developers.zomato.com/api/v2.1/cities?q=Austin&apikey=9de42e8f38d437b717a205e52e647b2f
+// //==========================================zomatoAPI==================================//
+// // restaurant name
+// // **** city/loacation
+// var location = "austin"
+// // hours
+// // **** cuisine type
+// var cuisineType = "" 
+// // **** price range 
+// var price = ""
+// // **** meal type (breakfast/lunch/dinner)
+// var mealType = ""
+// // view menu
+// // review score
+// // featured tags
+// var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=" + location + "&apikey=9de42e8f38d437b717a205e52e647b2f"
+// //==========================================zomatoAPI==================================//
+
+
 // *** USE GOOGLE API TO GEOLOCATE BASED ON LATITUDE/LONGITUDE
+var queryURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCrdiVhj7Un_ACNVqMw9dozHxGNVglpwmo";
 
-// restaurant name
-// **** city/loacation
-var location = "austin"
-// hours
-// **** cuisine type
-var cuisineType = "" 
-// **** price range 
-var price = ""
-// **** meal type (breakfast/lunch/dinner)
-var mealType = ""
-// view menu
-// review score
-// featured tags
-
-var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=" + location + "&apikey=9de42e8f38d437b717a205e52e647b2f"
 
 $.ajax({
     url: queryURL,
