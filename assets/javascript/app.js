4b4d1da09590b97457c2a36c971bdde4a240c342
Stephen McMillen <Shtevetm@gmail.com>
2019-09-20 11:22:02 -0500

    removed yelp API - Yelp website says it does not function with AJAX

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,21 @@
-<<<<<<< HEAD
+
+// var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=2405+Robert+Dedman+Dr&key=AIzaSyDj2CsSJ_HaADu_VP_2Q66zI34V9hYh5EI";
+var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=Austin,_TX&apikey=9de42e8f38d437b717a205e52e647b2f"
+
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+  
+
+
+
+
 //
-// https://developers.zomato.com/api/v2.1/cities?q=Austin&apikey=9de42e8f38d437b717a205e52e647b2f
+// var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=Austin&apikey=9de42e8f38d437b717a205e52e647b2f"
 // //==========================================zomatoAPI==================================//
 // // restaurant name
 // // **** city/loacation
@@ -17,31 +32,3 @@
 // // featured tags
 // var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=" + location + "&apikey=9de42e8f38d437b717a205e52e647b2f"
 // //==========================================zomatoAPI==================================//
-
-
-// *** USE GOOGLE API TO GEOLOCATE BASED ON LATITUDE/LONGITUDE
-var queryURL = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCrdiVhj7Un_ACNVqMw9dozHxGNVglpwmo";
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-})
-=======
-
-
-
-
-var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=2405+Robert+Dedman+Dr&key=AIzaSyDj2CsSJ_HaADu_VP_2Q66zI34V9hYh5EI";
-
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-  
->>>>>>> master
