2e052436890f1b81afe88255204dca1abd2dfb75
Thomas Rios <thomaserios@icloud.com>
2019-09-20 11:22:56 -0500

    cathcing up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,22 @@
+var queryURL = "https://api.ipdata.co?api-key=f754718a6a805ba8f15448bac5bf5e48e82b8c6b7b923fe91c9381eb"
 
 
+var queryURL2 ="https://maps.googleapis.com/maps/api/geocode/json?address=2405+Robert+Dedman+Dr&key=AIzaSyDj2CsSJ_HaADu_VP_2Q66zI34V9hYh5EI";
 
-
-var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=2405+Robert+Dedman+Dr&key=AIzaSyDj2CsSJ_HaADu_VP_2Q66zI34V9hYh5EI";
 
 
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function(response) {
+}).then(function (response) {
   console.log(response);
 });
-  
+
+$.ajax({
+  url: queryURL2,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+});
+
+
